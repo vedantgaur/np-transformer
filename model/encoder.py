8c80980dbeab76fd4213c7025df1c0e667516409
Vedant Gaur <vedantgaur@Vedants-MacBook-Pro.local>
2025-01-05 17:00:02 -0800

    finished encoder and ffn

---
+++ model/encoder.py
@@ -0,0 +1,29 @@
+import numpy as np
+
+from attention import multi_headed_attn
+from feedforward import ff
+
+def encoder(x, d_ff, d_model, h):
+    W_Q = np.random.randn(d_model, d_model // h)
+    W_K = np.random.randn(d_model, d_model // h)
+    W_V = np.random.randn(d_model, d_model // h)
+
+    Q = x @ W_Q
+    K = x @ W_K
+    V = x @ W_V
+
+    attn = multi_headed_attn(Q, K, V, h)
+    layer_norm_attn = layer_norm(x + attn)
+
+    ffn_output = ff(layer_norm_attn, d_ff, d_model)
+    
+    layer_norm_ffn = layer_norm(layer_norm_attn + ffn_output)
+
+    return layer_norm_ffn
+
+def layer_norm(x, gamma=1, beta=0):
+    mean = np.mean(x, axis=-1, keepdims=True)
+    var = np.var(x, axis=-1, keepdims=True)
+
+    x_norm = (x - mean) / np.sqrt(var + 1e-8)
+    return x_norm*gamma + beta
--- model/feedforward.py
+++ model/feedforward.py
@@ -1,2 +1,14 @@
-def ff(batch_size, seq_len, d_model):
-    pass
+import numpy as np
+
+def ff(x, d_ff, d_model):
+    W_1 = np.random.randn(d_model, d_ff)
+    b_1 = np.random.randn(d_ff)
+
+    x = x @ W_1 + b_1
+    x = np.maximum(0, x)
+
+    W_2 = np.random.randn(d_ff, d_model)
+    b_2 = np.random.randn(d_model)
+    x = x @ W_2 + b_2
+
+    return x
